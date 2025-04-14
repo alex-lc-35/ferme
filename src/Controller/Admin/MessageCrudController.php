1de08cb1fdda993a437d10c57f811a0bec60f073
ThomasdevLC <thomas.lecam@gmail.com>
2025-04-14 11:09:27 +0200

    disableOtherMessages method refacto in service

--- src/Controller/Admin/MessageCrudController.php
+++ src/Controller/Admin/MessageCrudController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Message;
 use App\Enum\MessageType;
-use App\Repository\MessageRepository;
+use App\Service\MessageService;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\{
@@ -14,17 +14,26 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\{
     TextField,
 };
 use Symfony\Bundle\SecurityBundle\Security;
-
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 class MessageCrudController extends AbstractCrudController
 {
     public function __construct(
         private Security $security,
-        private MessageRepository $messageRepository
+        private MessageService $messageService
     ) {}
 
     public static function getEntityFqcn(): string
     {
         return Message::class;
+    }
+
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Message')
+            ->setEntityLabelInPlural('Messages')
+            ->setDefaultSort(['type' => 'ASC']);
     }
 
     public function configureFields(string $pageName): iterable
@@ -73,42 +82,20 @@ class MessageCrudController extends AbstractCrudController
         return $message;
     }
 
-    // Surcharge pour persister le message (formulaire complet)
     public function persistEntity(EntityManagerInterface $em, $entityInstance): void
     {
         if ($entityInstance instanceof Message && $entityInstance->isActive()) {
-            $this->disableOtherMessages($em, $entityInstance);
+            $this->messageService->disableOtherMessages($entityInstance);
         }
         parent::persistEntity($em, $entityInstance);
     }
 
-    // Surcharge pour mettre à jour le message
     public function updateEntity(EntityManagerInterface $em, $entityInstance): void
     {
         if ($entityInstance instanceof Message && $entityInstance->isActive()) {
-            $this->disableOtherMessages($em, $entityInstance);
+            $this->messageService->disableOtherMessages($entityInstance);
         }
         parent::updateEntity($em, $entityInstance);
-    }
-
-
-    private function disableOtherMessages(EntityManagerInterface $em, Message $current): void
-    {
-        $qb = $this->messageRepository->createQueryBuilder('m')
-            ->where('m.type = :type')
-            ->andWhere('m.id != :id')
-            ->andWhere('m.isActive = true');
-
-        $qb->setParameter('type', $current->getType());
-        $qb->setParameter('id', $current->getId() ?? 0);
-
-        $others = $qb->getQuery()->getResult();
-
-
-        foreach ($others as $other) {
-            $other->setIsActive(false);
-            $em->persist($other);
-        }
     }
 
 }
