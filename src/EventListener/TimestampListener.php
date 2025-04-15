de8927e4f72a6e894375ca03f22c1caa2725c128
ThomasdevLC <thomas.lecam@gmail.com>
2025-04-15 16:42:41 +0200

    timestamp listener

---
+++ src/EventListener/TimestampListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventListener;
+
+
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+/**
+*  listener s'applique  à l'entité Product.
+*/
+#[AsEntityListener(event: 'prePersist', method: 'prePersist', entity: Product::class)]
+#[AsEntityListener(event: 'preUpdate', method: 'preUpdate', entity: Product::class)]
+class TimestampListener
+{
+public function prePersist(Product $product, PrePersistEventArgs $event): void
+{
+$now = new \DateTimeImmutable();
+
+if (null === $product->getCreatedAt()) {
+$product->setCreatedAt($now);
+}
+
+$product->setUpdatedAt(new \DateTime());
+}
+
+public function preUpdate(Product $product, PreUpdateEventArgs $event): void
+{
+$product->setUpdatedAt(new \DateTime());
+}
+}
