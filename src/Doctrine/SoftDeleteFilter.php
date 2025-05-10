b64592aa1c52d989d122daefd483015035bef958
AlexandreLeCam <a.lecam@myanapro.com>
2025-05-10 16:00:15 +0200

    docker prod V1

--- src/Doctrine/SoftDeleteFilter.php
+++ src/Doctrine/SoftDeleteFilter.php
@@ -1,19 +1,19 @@
 <?php
-//
-//namespace App\Doctrine;
-//
-//use Doctrine\ORM\Mapping\ClassMetadata;
-//use Doctrine\ORM\Query\Filter\SQLFilter;
-//
-//class SoftDeleteFilter extends SQLFilter
-//{
-//    public function addFilterConstraint(ClassMetadata $targetEntity, string $targetTableAlias): string
-//    {
-//        if (!$targetEntity->reflClass->hasProperty('isDeleted')) {
-//            return '';
-//        }
-//
-//        return sprintf('%s.is_deleted = 0', $targetTableAlias);
-//    }
-//
-//}
+
+namespace App\Doctrine;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Query\Filter\SQLFilter;
+
+class SoftDeleteFilter extends SQLFilter
+{
+    public function addFilterConstraint(ClassMetadata $targetEntity, string $targetTableAlias): string
+    {
+        if (!$targetEntity->reflClass->hasProperty('isDeleted')) {
+            return '';
+        }
+
+        return sprintf('%s.is_deleted = 0', $targetTableAlias);
+    }
+
+}
