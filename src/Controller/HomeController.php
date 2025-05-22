3ab4c90efe6e66d9d4b747cf102e65699fcaf85a
AlexandreLeCam <a.lecam@myanapro.com>
2025-05-22 09:27:20 +0200

    debug csrf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, CsrfTokenManagerInterface $csrfTokenManager): Response
     {
+        $request->getSession()->set('test_csrf', 'ok');
+        // dd($request->getSession()->getId());
+
+        $message = null;
+
+        if ($request->isMethod('POST')) {
+            $submittedToken = $request->request->get('_token');
+
+            if ($csrfTokenManager->isTokenValid(new CsrfToken('test-form', $submittedToken))) {
+                $message = '✅ Jeton CSRF valide.';
+            } else {
+                $message = '❌ Jeton CSRF invalide !';
+            }
+        }
+
+        $csrfToken = $csrfTokenManager->getToken('test-form')->getValue();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'csrf_token' => $csrfToken,
+            'message' => $message,
         ]);
     }
 }
